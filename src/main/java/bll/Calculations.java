58f436cba448836dbed8cc998564be6f5686835f
kubaczechowski <jcobczechowski@gmail.com>
2021-03-22 20:08:19 +0100

    bll interface

---
+++ src/main/java/bll/Calculations.java
@@ -0,0 +1,8 @@
+package bll;
+
+/**
+ * @author Kuba
+ * @date 3/22/2021 5:32 PM
+ */
+public class Calculations {
+}
---
+++ src/main/java/bll/ICalculations.java
@@ -0,0 +1,9 @@
+package bll;
+
+/**
+ * @author Kuba
+ * @date 3/22/2021 5:32 PM
+ */
+public interface ICalculations {
+        void getOverviewClassAttendance();
+}
