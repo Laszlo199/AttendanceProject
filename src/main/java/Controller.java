e0e0eabfeb70edb0234be5129bf88ebb25a288c1
kubaczechowski <jcobczechowski@gmail.com>
2021-03-22 16:16:33 +0100

    set up dependencies

---
+++ src/main/java/Controller.java
@@ -0,0 +1,15 @@
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * @author Kuba
+ * @date 3/22/2021 4:06 PM
+ */
+public class Controller implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        System.out.println("It is working....");
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,24 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * @author Kuba
+ * @date 3/22/2021 3:08 PM
+ */
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("sample.fxml"));
+        Parent root = loader.load();
+        stage.setTitle("MyTunes");
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+}
