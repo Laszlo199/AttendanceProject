b66e051879c241e079e9e0b5c550a52b4f855319
kubaczechowski <jcobczechowski@gmail.com>
2021-03-23 18:05:07 +0100

    Update DB connector

--- src/main/java/dal/DBConnector.java
+++ src/main/java/dal/DBConnector.java
@@ -1,6 +1,38 @@
 package dal;
 
-public class DBConnector {
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
 
-    SQLServerDataSource dataSource;
+import java.sql.Connection;
+
+public class DBConnector {
+    private SQLServerDataSource dataSource;
+
+    public DBConnector()
+    {
+        //kamilas DB
+
+        /*
+        dataSource = new SQLServerDataSource();
+        dataSource.setServerName("10.176.111.31");
+        dataSource.setUser("CSe20B_10");
+        dataSource.setPassword("database000");
+        dataSource.setDatabaseName("ItunesGROUP2");
+
+         */
+
+
+        dataSource = new SQLServerDataSource();
+        dataSource.setServerName("10.176.111.31");
+        dataSource.setUser("CSe20B_8");
+        dataSource.setPassword("potatoe2021");
+        dataSource.setDatabaseName("MyItuesKuba");
+
+
+    }
+
+    public Connection getConnection() throws SQLServerException
+    {
+        return dataSource.getConnection();
+    }
 }
