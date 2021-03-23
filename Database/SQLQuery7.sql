79d026853af911c22ad51d505e15723462bcf27b
soma199 <70133057+Laszlo199@users.noreply.github.com>
2021-03-23 10:07:41 +0100

    identity

--- Database/SQLQuery7.sql
+++ Database/SQLQuery7.sql
@@ -1,60 +1,54 @@
 CREATE TABLE Subjects (
-    ID int  NOT NULL,
+    ID int IDENTITY(1,1) PRIMARY KEY,
     [Name] nvarchar(50) NOT NULL,
 	TeacherID int NOT NULL,
-    PRIMARY KEY (ID),
 	FOREIGN KEY (TeacherID) REFERENCES Teachers(ID)
 );
 
 CREATE TABLE Teachers (
-    ID int  NOT NULL,
+    ID int  IDENTITY(1,1) PRIMARY KEY,
     [Name] nvarchar(50) NOT NULL,
     Email nvarchar(50) NOT NULL,
     SubjectID int NOT NULL,
 	Department nvarchar(50) NOT NULL,
 	PhotoPath nvarchar(50),
-	PRIMARY KEY (ID),
 	FOREIGN KEY (SubjectID) REFERENCES Subjects(ID)
-	
-	
+
 );
 
 
 CREATE TABLE Courses(
-    ID int  NOT NULL,
+    ID int  IDENTITY(1,1) PRIMARY KEY,
     [Name] nvarchar(50) NOT NULL,
-    PRIMARY KEY (ID) 
+
 );
 
 
 CREATE TABLE Students (
-    ID int  NOT NULL,
+    ID int IDENTITY(1,1) PRIMARY KEY,
     [Name] nvarchar(50) NOT NULL,
     Email nvarchar(50) NOT NULL,
     CourseID int NOT NULL,
 	Semester int NOT NULL,
 	PhotoPath nvarchar(50),
-	PRIMARY KEY (ID), 
 	FOREIGN KEY (CourseID) REFERENCES Courses(ID)
 );
 
 CREATE TABLE ScheduleEntity(
-    ID int  NOT NULL,
+    ID int IDENTITY(1,1) PRIMARY KEY,
     [WeekDay] nvarchar(50) NOT NULL,
 	StartTime dateTime NOT NULL,
 	EndTime dateTime NOT NULL,
 	SubjectID int,
-    PRIMARY KEY (ID),
 	FOREIGN KEY (SubjectID) REFERENCES Subjects(ID)
 );
 
 CREATE TABLE Records(
-    ID int  NOT NULL,
+    ID int IDENTITY(1,1) PRIMARY KEY,
     StudentID int NOT NULL,
 	ScheduleEntityID int NOT NULL,
 	isPresent bit,
 	[Date] datetime,
-    PRIMARY KEY (ID), 
 	FOREIGN KEY (StudentID) REFERENCES Students(ID),
 	FOREIGN KEY (ScheduleEntityID) REFERENCES ScheduleEntity(ID)
 );
