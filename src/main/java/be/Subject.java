c85c42b370c78fb51f92f695cb86bc568391e05d
Kamila Potasiak <kami7686@easv365.dk>
2021-03-24 16:42:31 +0100

    update in courseId in Subject

--- src/main/java/be/Subject.java
+++ src/main/java/be/Subject.java
@@ -5,11 +5,13 @@ public class Subject {
     private int id;
     private String name;
     private int teacherId;
+    private int courseId;
 
-    public Subject(int id, String name, int teacherId) {
+    public Subject(int id, String name, int teacherId, int courseId) {
         this.id = id;
         this.name = name;
         this.teacherId = teacherId;
+        this.courseId = courseId;
     }
 
     public int getId() {
@@ -34,5 +36,13 @@ public class Subject {
 
     public void setTeacherId(int teacherId) {
         this.teacherId = teacherId;
+    }
+
+    public int getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(int courseId) {
+        this.courseId = courseId;
     }
 }
