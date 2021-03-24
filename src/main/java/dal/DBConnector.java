e5780a6c9c90e0082e6e417166c13283aa4261df
kubaczechowski <jcobczechowski@gmail.com>
2021-03-24 13:32:50 +0100

    Update Dbconnector

--- src/main/java/dal/DBConnector.java
+++ src/main/java/dal/DBConnector.java
@@ -4,6 +4,7 @@ import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
 import com.microsoft.sqlserver.jdbc.SQLServerException;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
 public class DBConnector {
     private SQLServerDataSource dataSource;
@@ -15,11 +16,21 @@ public class DBConnector {
         dataSource.setUser("CSe20B_8");
         dataSource.setPassword("potatoe2021");
         dataSource.setDatabaseName("AttendanceProject");
-
     }
 
     public Connection getConnection() throws SQLServerException
     {
         return dataSource.getConnection();
+    }
+
+    public boolean isDbConnected() {
+        try {
+            return getConnection() != null && !getConnection().isClosed();
+        } catch (SQLServerException throwables) {
+            throwables.printStackTrace();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return false;
     }
 }
