a03aa800b03b5a47cc2c3a1fe24cdfa56457799b
kubaczechowski <jcobczechowski@gmail.com>
2021-03-26 16:58:00 +0100

    Preferences functionality finished

--- src/main/java/gui/controller/LogInWindowController.java
+++ src/main/java/gui/controller/LogInWindowController.java
@@ -17,6 +17,7 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.prefs.BackingStoreException;
 import java.util.prefs.Preferences;
 
 /**
@@ -26,6 +27,12 @@ import java.util.prefs.Preferences;
 public class LogInWindowController implements Initializable, ILogIn {
     Preferences pref = Preferences.userNodeForPackage(LogInWindowController.class);
     private LoginModel loginModel = LoginModel.getInstance();
+    private boolean rememberMecheck ;
+    private static final String PASSWORD = "password";
+    private static final String EMAIL = "email";
+    private static final String EMPTY = "empty";
+    private static final int DEFAULTLENGTH  = 0;
+    private static final String PASSWORDL = "passwordLength";
 
 
     @FXML
@@ -50,6 +57,7 @@ public class LogInWindowController implements Initializable, ILogIn {
         String password  = passwordField.getText();
         if(!email.isEmpty() && !password.isEmpty()){
             pref.put("email", emailField.getText());
+            pref.putInt(PASSWORDL, password.length());
             String hashedPassword = loginModel.getHashedPassword(password);
             pref.put("password", hashedPassword );
         }
@@ -63,8 +71,12 @@ public class LogInWindowController implements Initializable, ILogIn {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        /*
+        rememberMecheck = rememberMe.isSelected();
+        rememberMe.setDisable(true);
+        checkPreferences();
+        rememberMe.setDisable(false);
 
+/*
         String salt = PasswordHasher.generateSalt(512).get();
         String testPass = "blabla";
 
@@ -75,7 +87,31 @@ public class LogInWindowController implements Initializable, ILogIn {
                 key, salt);
         System.out.println("password is:" +verify);
 
-         */
+ */
+    }
+
+    /**
+     * when program is run it checks whether preferences are saved
+     * in a file
+     */
+    private void checkPreferences() {
+       String email = pref.get(EMAIL, EMPTY);
+       String hashedPassword = pref.get(PASSWORD, EMPTY);
+       if(!email.equals(EMPTY) && !hashedPassword.equals(EMPTY)){
+           rememberMe.setSelected(true);
+           emailField.setText(email);
+           String passTemp = getStringWithPasswordLength();
+           passwordField.setText(passTemp);
+       }
+    }
+
+    private String getStringWithPasswordLength() {
+        String temp = "";
+       int iterations = pref.getInt(PASSWORDL, 0);
+       for(int i=0; i< iterations; i++)
+           temp+="a";
+
+       return temp;
     }
 
 
@@ -84,6 +120,8 @@ public class LogInWindowController implements Initializable, ILogIn {
      * @param actionEvent
      */
     public void logTeacherButton(ActionEvent actionEvent) {
+        //if preferences are set remember to check the value from preferences
+        // not test because it actually not exists
     }
 
     /**
@@ -91,5 +129,23 @@ public class LogInWindowController implements Initializable, ILogIn {
      * @param actionEvent
      */
     public void logStudentButton(ActionEvent actionEvent) {
+    }
+
+    /**
+     * method handles check box for saving user in preferences
+     * @param actionEvent
+     */
+    public void rememberMe(ActionEvent actionEvent) {
+        if(!passwordField.getText().isEmpty() &&
+        !emailField.getText().isEmpty()){
+            if(rememberMecheck ==false){
+                saveUserInPreferences();
+                rememberMecheck = true;
+            }
+            else{
+                unsaveUserInPreferences();
+                rememberMecheck = false;
+            }
+        }
     }
 }
