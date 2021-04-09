8a5c5226d7b6d6e24a5ac06825cbf8b735c1e628
soma199 <70133057+Laszlo199@users.noreply.github.com>
2021-04-09 13:07:00 +0200

    AdminModel

--- src/main/java/gui/model/AdminModel.java
+++ src/main/java/gui/model/AdminModel.java
@@ -4,13 +4,29 @@ import be.Course;
 import be.Student;
 import be.Subject;
 import be.Teacher;
+import bll.IFacadeBLL;
+import bll.exception.BLLexception;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
 public class AdminModel {
+    IFacadeBLL iFacadeBLL;
+
+
     private ObservableList<Teacher> obsTeacher;
     private ObservableList<Student> obsStudent;
     private ObservableList<Subject> obsSubject;
     private ObservableList<Course> obsCourse;
+
+
+
+    private AdminModel() {
+        obsTeacher = FXCollections.observableArrayList();
+        obsStudent= FXCollections.observableArrayList();
+        obsSubject = FXCollections.observableArrayList();
+        obsCourse = FXCollections.observableArrayList();
+
+    }
 
 
 
@@ -20,17 +36,41 @@ public class AdminModel {
         return obsTeacher;
     }
     public void loadTeachers() {
-
+        obsTeacher.clear();
+        try{
+            obsTeacher.addAll(iFacadeBLL.getAllTeacher());
+        }catch (BLLexception blLexception){
+            blLexception.printStackTrace();
+        }
     }
     public void delete(Teacher selectedTeacher) {
-
+        try{
+          iFacadeBLL.deleteTeacher(selectedTeacher);
+        }catch (BLLexception blLexception){
+            blLexception.printStackTrace();
+        }
+        obsTeacher.remove(selectedTeacher);
     }
+
     public void save(Teacher teacher) {
-
+        try{
+            iFacadeBLL.createTeacher(teacher);
+        }catch (BLLexception blLexception){
+            blLexception.printStackTrace();
+        }
+        obsTeacher.add(teacher);
     }
+
     public void updateTeacher(Teacher oldTeacher, Teacher newTeacher){
-
+        try{
+            iFacadeBLL.updateTeacher(oldTeacher,newTeacher);
+        }catch (BLLexception blLexception){
+            blLexception.printStackTrace();
+        }
+        obsTeacher.clear();
+        obsTeacher.addAll();
     }
+
 
 
     //Students---------------------
@@ -38,17 +78,42 @@ public class AdminModel {
 
         return obsStudent;
     }
+
     public void loadStudents() {
-
+        obsStudent.clear();
+        try{
+            obsStudent.addAll(iFacadeBLL.getAllStudent());
+        }catch (BLLexception blLexception){
+            blLexception.printStackTrace();
+        }
     }
+
     public void delete(Student selectedStudent) {
-
+        try{
+            iFacadeBLL.deleteStudent(selectedStudent);
+        }catch (BLLexception blLexception){
+            blLexception.printStackTrace();
+        }
+        obsStudent.remove(selectedStudent);
     }
+
     public void save(Student student) {
-
+        try{
+            iFacadeBLL.createStudent(student);
+        }catch (BLLexception blLexception){
+            blLexception.printStackTrace();
+        }
+        obsStudent.add(student);
     }
-    public void updateStudent(Student oldStudent, Student newStudent){
 
+    public void updateStudent(Student oldStudent, Student newStudent){
+        try{
+            iFacadeBLL.updateStudent(oldStudent,newStudent);
+        }catch (BLLexception blLexception){
+            blLexception.printStackTrace();
+        }
+        obsStudent.clear();
+        obsStudent.addAll();
     }
 
 
@@ -58,17 +123,42 @@ public class AdminModel {
 
         return obsSubject;
     }
+
     public void loadSubjects() {
-
+        obsSubject.clear();
+        try{
+            obsSubject.addAll(iFacadeBLL.getAllSubject());
+        }catch (BLLexception blLexception){
+            blLexception.printStackTrace();
+        }
     }
+
     public void delete(Subject selectedSubject) {
-
+        try{
+            iFacadeBLL.deleteSubject(selectedSubject);
+        }catch (BLLexception blLexception){
+            blLexception.printStackTrace();
+        }
+        obsSubject.remove(selectedSubject);
     }
+
     public void save(Subject subject) {
-
+        try{
+            iFacadeBLL.createSubject(subject);
+        }catch (BLLexception blLexception){
+            blLexception.printStackTrace();
+        }
+        obsSubject.add(subject);
     }
-    public void updateSubject(Subject oldSubject, Subject newSubject){
 
+    public void updateSubject(Subject oldSubject, Subject newSubject){
+        try{
+            iFacadeBLL.updateSubject(oldSubject,newSubject);
+        }catch (BLLexception blLexception){
+            blLexception.printStackTrace();
+        }
+        obsSubject.clear();
+        obsSubject.addAll();
     }
 
 
@@ -78,16 +168,42 @@ public class AdminModel {
 
         return obsCourse;
     }
+
     public void loadCourses() {
-
+        obsCourse.addAll();
+        try{
+            obsCourse.addAll(iFacadeBLL.getAllCourse());
+        }catch (BLLexception blLexception){
+            blLexception.printStackTrace();
+        }
     }
+
     public void delete(Course selectedCourse) {
-
+        try{
+            iFacadeBLL.deleteCourse(selectedCourse);
+        }catch (BLLexception blLexception){
+            blLexception.printStackTrace();
+        }
+       obsCourse.remove(selectedCourse);
     }
+
     public void save(Course course) {
-
+        try{
+            iFacadeBLL.createCourse(course);
+        }catch (BLLexception blLexception){
+            blLexception.printStackTrace();
+        }
+        obsCourse.add(course);
     }
+
     public void updateCourse(Course oldCourse, Course newCourse){
-
+        try{
+            iFacadeBLL.updateCourse(oldCourse,newCourse);
+        }catch (BLLexception blLexception){
+            blLexception.printStackTrace();
+        }
+        obsCourse.clear();
+        obsCourse.addAll();
     }
+
 }
